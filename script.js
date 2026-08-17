d2cb0ef541c48597aa4fd1e596a663157460dd7b
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-17 18:20:36 +0530

    Done changes

--- script.js
+++ script.js
@@ -32,11 +32,11 @@
 // firstPageAnim();
 
 function circleMouseFollower(xscale, yscale) {
-  window.addEventListener("mousemove", function (dets) {
-    document.querySelector(
-      "#minicircle"
-    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
-  });
+  // window.addEventListener("mousemove", function (dets) {
+  //   document.querySelector(
+  //     "#minicircle"
+  //   ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+  // });
 }
 circleMouseFollower();
 
